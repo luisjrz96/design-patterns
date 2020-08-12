23af1d375300d4247dc0f48f1b4d4c03f7103254
Luis Juarez <luisjrz96@hotmail.com>
2020-08-11 21:11:22 -0500

    initializing attributes in the constructor

--- src/main/java/creational/Factory/product/Character.java
+++ src/main/java/creational/Factory/product/Character.java
@@ -2,8 +2,12 @@ package creational.Factory.product;
 
 public abstract class Character {
 	
-	private boolean alive = true;
-
+	private boolean alive;
+	
+	public Character() {
+		alive = true;
+	}
+	
 	public abstract void attack();
 	public abstract void useSpecialAbility();
 	public abstract void regenerate();
